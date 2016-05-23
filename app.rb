82dd9c6b4b2c2f90d8bd185c4b4c85cd9d9fdca4
Yury Layeuski <ylayeuski@ylayeuski-mbp.local>
2016-05-22 22:52:26 -0700

    Add Rakefile

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
 set :database, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base 
+end
 
 
 get '/' do
