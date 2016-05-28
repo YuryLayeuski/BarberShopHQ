997a0f9fc4a003a65da13bcaf350ff6eb8a7954a
Yury Layeuski <ylayeuski@ylayeuski-mbp.local>
2016-05-28 15:05:33 -0700

    Keep values on post

--- app.rb
+++ app.rb
@@ -24,17 +24,21 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
 
-	c = Client.new params[:client]
-	c.save
-
-	erb "<h2>Thank You!!! You are complete reservation!!!</h2>"
-
+	@c = Client.new params[:client]
+	if @c.save
+		erb "<h2>Thank You!!! You are complete reservation!!!</h2>"
+	else
+		@error = @c.errors.full_messages.first
+		erb :visit 
+	end
 end
+
 
 
 
